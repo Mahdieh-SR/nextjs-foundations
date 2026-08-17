5298a24815288d61b28dfe67ce562c87ca337fba
Madi <mahdiehsrwork@gmail.com>
2026-08-17 11:05:14 +0300

    feat: add proxy with security headers and request logging

---
+++ apps/web/src/proxy.ts
@@ -0,0 +1,25 @@
+import { type NextRequest, NextResponse } from 'next/server'
+
+export function proxy(request: NextRequest) {
+  console.log(`[Proxy] ${request.method} ${request.nextUrl.pathname}`)
+
+  const requestHeaders = new Headers(request.headers)
+  requestHeaders.set('x-request-id', crypto.randomUUID())
+  requestHeaders.set('x-pathname', request.nextUrl.pathname)
+
+  const response = NextResponse.next({
+    request: {
+      headers: requestHeaders,
+    },
+  })
+
+  response.headers.set('X-Frame-Options', 'DENY')
+  response.headers.set('X-Content-Type-Options', 'nosniff')
+  response.headers.set('X-Request-Id', requestHeaders.get('x-request-id') || '')
+
+  return response
+}
+
+export const config = {
+  matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
+}
